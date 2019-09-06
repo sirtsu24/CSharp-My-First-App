18c7786ff09215c6f781f5491bd687d01e69c1c8
Opilane TTHK <opilane@tthk.ee>
2019-09-06 17:03:16 +0300

    My first commit

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -7,6 +7,11 @@ namespace MyApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine("Hello " +  name);
+            Console.ReadLine();
         }
     }
 }
